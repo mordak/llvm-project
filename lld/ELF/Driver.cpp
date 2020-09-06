3fcaa1eaac0ba7628e29b99f9c8ecd27f520b9d9
Todd Mortimer <todd@opennet.ca>
2020-09-06 16:38:37 -0400

    Remove redundant case. Noticed by Brad.

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -142,7 +142,6 @@ static std::tuple<ELFKind, uint16_t, uint8_t> parseEmulation(StringRef emul) {
           .Cases("elf32ltsmip", "elf32ltsmipn32", {ELF32LEKind, EM_MIPS})
           .Case("elf32lriscv", {ELF32LEKind, EM_RISCV})
           .Cases("elf32ppc", "elf32ppclinux", {ELF32BEKind, EM_PPC})
-          .Case("elf64_sparc", {ELF64BEKind, EM_SPARCV9})
           .Case("elf64btsmip", {ELF64BEKind, EM_MIPS})
           .Case("elf64ltsmip", {ELF64LEKind, EM_MIPS})
           .Case("elf64lriscv", {ELF64LEKind, EM_RISCV})
