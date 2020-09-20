7e5378ae99935789412b72512450a5d0a66fdb5a
Todd Mortimer <todd@opennet.ca>
2020-09-20 19:31:59 -0400

    Revert "Set max atomic size for PowerPC."
    This reverts commit 5bbf1c81df59fb23334e2b1ce764ac2369bfe702.
    
    Fixed upstream.

--- llvm/lib/Target/PowerPC/PPCISelLowering.cpp
+++ llvm/lib/Target/PowerPC/PPCISelLowering.cpp
@@ -1184,10 +1184,7 @@ PPCTargetLowering::PPCTargetLowering(const PPCTargetMachine &TM,
 
   setOperationAction(ISD::READCYCLECOUNTER, MVT::i64, isPPC64 ? Legal : Custom);
 
-  if (isPPC64)
-    setMaxAtomicSizeInBitsSupported(64);
-  else {
-    setMaxAtomicSizeInBitsSupported(32);
+  if (!isPPC64) {
     setOperationAction(ISD::ATOMIC_LOAD,  MVT::i64, Expand);
     setOperationAction(ISD::ATOMIC_STORE, MVT::i64, Expand);
   }
