91eaa9aa3bbd4421d6d17664b3d188d98c47d27b
Todd Mortimer <todd@opennet.ca>
2020-10-12 18:22:28 -0400

    Add RETGUARD implementation for powerpc and powerpc64.
    ok deraadt@ kettenis@

--- llvm/lib/Target/PowerPC/PPCFrameLowering.cpp
+++ llvm/lib/Target/PowerPC/PPCFrameLowering.cpp
@@ -15,6 +15,7 @@
 #include "PPCInstrBuilder.h"
 #include "PPCInstrInfo.h"
 #include "PPCMachineFunctionInfo.h"
+#include "PPCReturnProtectorLowering.h"
 #include "PPCSubtarget.h"
 #include "PPCTargetMachine.h"
 #include "llvm/ADT/Statistic.h"
@@ -2414,4 +2415,8 @@ bool PPCFrameLowering::enableShrinkWrapping(const MachineFunction &MF) const {
     return false;
   return (MF.getSubtarget<PPCSubtarget>().isSVR4ABI() &&
           MF.getSubtarget<PPCSubtarget>().isPPC64());
+}
+
+const ReturnProtectorLowering *PPCFrameLowering::getReturnProtector() const {
+  return &RPL;
 }
