ddb1ee58a4290cb862180ba6322b94610ceceed1
deraadt <deraadt@openbsd.org>
2022-10-07 15:04:51 +0000

    In the linkers, collect objects in section "openbsd.mutable" and place them into a page-aligned region in the bss, with the right markers for kernel/ld.so to identify the region and skip making it immutable. While here, fix readelf/objdump versions to show all of this. ok miod kettenis

--- lld/ELF/LinkerScript.cpp
+++ lld/ELF/LinkerScript.cpp
@@ -107,7 +107,7 @@ static StringRef getOutputSectionName(const InputSectionBase *s) {
        {".data.rel.ro", ".data", ".rodata", ".bss.rel.ro", ".bss",
         ".gcc_except_table", ".init_array", ".fini_array", ".tbss", ".tdata",
         ".ARM.exidx", ".ARM.extab", ".ctors", ".dtors",
-        ".openbsd.randomdata."})
+        ".openbsd.randomdata.", ".openbsd.mutable."})
     if (isSectionPrefix(v, s->name))
       return v;
 
