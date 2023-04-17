f0e83d9b8db796268a0390d3195d17986a0cf9e2
kettenis <kettenis@openbsd.org>
2023-04-17 18:10:26 +0000

    Make -mbranch-protection=bti the default on OpenBSD.
    ok deraadt@

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1606,6 +1606,10 @@ static void CollectARMPACBTIOptions(const ToolChain &TC, const ArgList &Args,
         Args.MakeArgString(Twine("-msign-return-address-key=") + Key));
   if (IndirectBranches)
     CmdArgs.push_back("-mbranch-target-enforce");
+  } else {
+    if (Triple.isOSOpenBSD())
+      CmdArgs.push_back("-mbranch-target-enforce");
+  }
 }
 
 void Clang::AddARMTargetArgs(const llvm::Triple &Triple, const ArgList &Args,
